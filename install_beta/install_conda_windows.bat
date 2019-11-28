90fdcfc8aff5afb7849169dd529ed878d6ccc012
Ella Jameson <ellagjameson@gmail.com>
2019-11-27 21:19:12 -0700

    Update install_conda_windows.bat

--- install_beta/install_conda_windows.bat
+++ install_beta/install_conda_windows.bat
@@ -2,8 +2,8 @@
 
 REM DO NOT USE THIS SCRIPT. It is for the new Inno Setup installer.
 
-set "MINICONDAPATH="
-set "CONDAEXE="
+set MINICONDAPATH=%USERPROFILE%\Miniconda3
+set CONDAEXE=%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-condainstall.exe
 set "OS="
 set "MCLINK="
 
@@ -15,17 +15,13 @@ reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" 
 if %OS%==32BIT set MCLINK=https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86.exe
 if %OS%==64BIT set MCLINK=https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
 
-set CONDAEXE=%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-condainstall.exe
-
 echo Downloading Miniconda3 (This will take while, please wait)...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%MCLINK%', '%CONDAEXE%')" 
 if errorlevel 1 goto CONDAERROR
 
 echo Installing Miniconda3... (This will also take a while, please wait...)
-set MINICONDAPATH=%USERPROFILE%\Miniconda3
 start /wait /min "Installing Miniconda3..." "%CONDAEXE%" /InstallationType=JustMe /S /D="%MINICONDAPATH%"
 del "%CONDAEXE%"
-if errorlevel 1 goto CONDAERROR
 if not exist "%MINICONDAPATH%\" (goto CONDAERROR)
 
 "%MINICONDAPATH%\Scripts\conda.exe" init
