b0fea77108eb823c035d75a5b60969bec2a055d2
Ella Jameson <ellagjameson@gmail.com>
2019-11-27 20:34:08 -0700

    Update install_conda_windows.bat

--- install_beta/install_conda_windows.bat
+++ install_beta/install_conda_windows.bat
@@ -8,13 +8,7 @@ set "OS="
 set "MCLINK="
 
 where conda >nul 2>nul
-if %ERRORLEVEL% EQU 0 goto CONDADONE
-
-:NOCONDA
-set "AREYOUSURE="
-set /P AREYOUSURE=No conda found. Install Miniconda3 and LPHK? (Y/[N]): 
-if /I "%AREYOUSURE%" EQU "Y" goto INSTALLCONDA
-goto NOINSTALLCONDA
+if %ERRORLEVEL% EQU 0 goto CONDAFOUND
 
 :INSTALLCONDA
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
@@ -25,7 +19,7 @@ set CONDAEXE=%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-condainstall.exe
 
 echo Downloading Miniconda3 (This will take while, please wait)...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%MCLINK%', '%CONDAEXE%')" 
-if errorlevel 1 goto END
+if errorlevel 1 goto CONDAERROR
 
 echo Installing Miniconda3... (This will also take a while, please wait...)
 set MINICONDAPATH=%USERPROFILE%\Miniconda3
@@ -37,24 +31,16 @@ if not exist "%MINICONDAPATH%\" (goto CONDAERROR)
 "%MINICONDAPATH%\Scripts\conda.exe" init
 if errorlevel 1 goto CONDAERROR
 
-echo Miniconda3 has been installed...
-timeout 5
-start cmd /c "%0 %CONDAWASINSTALLEDFLAG%"
+echo Miniconda3 has been installed!
 goto END
 
 :CONDAERROR
 echo Miniconda3 install failed!
-goto END
+exit /B 1
 
-:CONDADONE
+:CONDAFOUND
 echo Conda is already installed!
-goto HARDEND
-
-:NOINSTALLCONDA
-echo Not installing MiniConda3, exiting...
 goto END
 
 :END
-pause
-
-:HARDEND
+exit /B 0
