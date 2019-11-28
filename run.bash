627080769acd5def863a0454fa8178eaa1135d69
nimaid <ellagjameson#gmail.com>
2019-11-27 19:54:29 -0700

    Added linux  file

--- run.bash
+++ run.bash
@@ -1,2 +1,5 @@
-#!/bin/bash
-conda activate LPHK && python ./LPHK.py && conda deactivate
+#!/bin/bash -i
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+conda activate LPHK
+python $DIR/LPHK.py
+conda deactivate
