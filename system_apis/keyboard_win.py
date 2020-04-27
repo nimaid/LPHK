fc18c715f809ebfe6397ab8deb0c100176bf373c
duncte123 <duncan.sterken123@gmail.com>
2020-04-27 10:06:45 +0200

    Add a lot of keycodes

--- system_apis/keyboard_win.py
+++ system_apis/keyboard_win.py
@@ -1,4 +1,3 @@
-from system_apis.keyboard_unix import sp as sp_base
 import ctypes
 from ctypes import wintypes
 
@@ -82,11 +81,95 @@ def release_key(hexKeyCode):
     user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
 
 
-def sp(name):
-    key = sp_base(name)
+# List of all codes for keys:
+# # msdn.microsoft.com/en-us/library/dd375731
+key_map = {
+    "0": 0x30,
+    "1": 0x31,
+    "2": 0x32,
+    "3": 0x33,
+    "4": 0x34,
+    "5": 0x35,
+    "6": 0x36,
+    "7": 0x37,
+    "8": 0x38,
+    "9": 0x39,
 
-    if key is not None:
-        return key.vk
+    "num0": 0x60,
+    "num1": 0x61,
+    "num2": 0x62,
+    "num3": 0x63,
+    "num4": 0x64,
+    "num5": 0x65,
+    "num6": 0x66,
+    "num7": 0x67,
+    "num8": 0x68,
+    "num9": 0x69,
+
+    "f1": 0x70,
+    "f2": 0x71,
+    "f3": 0x72,
+    "f4": 0x73,
+    "f5": 0x74,
+    "f6": 0x75,
+    "f7": 0x76,
+    "f8": 0x77,
+    "f9": 0x78,
+    "f10": 0x79,
+    "f11": 0x7A,
+    "f12": 0x7B,
+    "f13": 0x7C,
+    "f14": 0x7D,
+    "f15": 0x7E,
+    "f16": 0x7F,
+    "f17": 0x80,
+    "f18": 0x81,
+    "f19": 0x82,
+    "f20": 0x83,
+    "f21": 0x84,
+    "f22": 0x85,
+    "f23": 0x86,
+    "f24": 0x87,
+
+    "a": 0x41,
+    "b": 0x42,
+    "c": 0x43,
+    "d": 0x44,
+    "e": 0x45,
+    "f": 0x46,
+    "g": 0x47,
+    "h": 0x48,
+    "i": 0x49,
+    "j": 0x4A,
+    "k": 0x4B,
+    "l": 0x4C,
+    "m": 0x4D,
+    "n": 0x4E,
+    "o": 0x4F,
+    "p": 0x50,
+    "q": 0x51,
+    "r": 0x52,
+    "s": 0x53,
+    "t": 0x54,
+    "u": 0x55,
+    "v": 0x56,
+    "w": 0x57,
+    "x": 0x58,
+    "y": 0x59,
+    "z": 0x5A,
+
+    "space": 0x20,
+
+    "cmd": 0x5B,
+    "win": 0x5B,
+    "win_r": 0x5C,
+    "apps": 0x5D,
+}
+
+
+def sp(name):
+    if name in key_map:
+        return key_map[name]
 
     return None
 
