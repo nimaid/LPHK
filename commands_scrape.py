6992e784a3450fdee28bf329745c49409a4716b6
Steve-the-wonder-dog <69791362+Steve-the-wonder-dog@users.noreply.github.com>
2021-02-17 18:42:12 +0800

    Multiple fixes to command_scrape * Automatically finds the tesseract application in either of the default locations * Works on multi-monitor setups

--- commands_scrape.py
+++ commands_scrape.py
@@ -1,8 +1,17 @@
 # This module is VERY specific to Win32
-import command_base, ms, kb, scripts, variables, win32gui, commands_win32, PIL, pytesseract
+import os, command_base, ms, kb, scripts, variables, win32gui, commands_win32, PIL, pytesseract
 from constants import *
 
 LIB = "cmds_sscr" # name of this library (for logging)
+
+T_PATH = os.getenv('LOCALAPPDATA') + '/Tesseract-OCR/tesseract.exe'
+if not os.path.isfile(T_PATH):
+    T_PATH = os.getenv('PROGRAMFILES') + '/Tesseract-OCR/tesseract.exe'
+    if not os.path.isfile(T_PATH):
+        raise Exception("Tesseract OCR not installed or cannot be located")
+
+pytesseract.pytesseract.tesseract_cmd = T_PATH
+
 
 # ##################################################
 # ### CLASS S_OCR_FORM_TEXT                      ###
@@ -33,28 +42,23 @@ class Scrape_OCR_Form_Text(commands_win32.Command_Win32):
 
 
     def Process(self, btn, idx, split_line):
-        p_from = (self.Get_param(btn, 1), self.Get_param(btn, 2))
-        p_to = (self.Get_param(btn, 3), self.Get_param(btn, 4))         
+        p_from = (self.Get_param(btn, 1), self.Get_param(btn, 2))     # Get the from coords
+        p_to = (self.Get_param(btn, 3), self.Get_param(btn, 4))       # and the to coords     
             
-        hwnd = self.Get_param(btn, 6, win32gui.GetForegroundWindow())
-        state = self.restore_window(hwnd, True)
+        hwnd = self.Get_param(btn, 6, win32gui.GetForegroundWindow()) # get the windoe we're using
+        state = self.restore_window(hwnd, True)                       # restore the window in question and make it FG
         try: 
-            import time
-            time.sleep(1)
-            p_from = win32gui.ClientToScreen(hwnd, p_from)        
+            p_from = win32gui.ClientToScreen(hwnd, p_from)            # convert to screren coords  
             p_to = win32gui.ClientToScreen(hwnd, p_to)   
-            box = p_from + p_to
-            print("@@@", box)
-            image = PIL.ImageGrab.grab(bbox = box)  # @@@ bugfix https://github.com/python-pillow/Pillow/issues/1547
-            image.save('C:/temp/temp.png')
+            box = p_from + p_to                                       # make a tuple with both coords
+            image = PIL.ImageGrab.grab(bbox = box, all_screens=True)  # capture an image
+            #image.save('C:/temp/temp.png')
         finally:
-            self.reset_window(state)
+            self.reset_window(state)                                  # restore windows something like previous states
 
-        pytesseract.pytesseract.tesseract_cmd = r'C:\Users\HE123240\AppData\Local\Tesseract-OCR\tesseract.exe'
-        txt = pytesseract.image_to_string(image)
-        print("@@@", txt)
+        txt = pytesseract.image_to_string(image)                      # OCR the image
             
-        self.Set_param(btn, 5, txt)                                  # pass this back            
+        self.Set_param(btn, 5, txt)                                   # pass the text back            
 
         
 scripts.Add_command(Scrape_OCR_Form_Text())  # register the command
