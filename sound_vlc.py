6e66ce41d754ee39beae845b5c08693433f601c2
Maximilian B <43041954+Clafter@users.noreply.github.com>
2024-02-08 14:03:33 +0100

    performance fix

--- sound_vlc.py
+++ sound_vlc.py
@@ -7,10 +7,11 @@ SOUND_PATH = "/user_sounds/"
 PATH = None
 
 # Create a new instance of the VLC player
-instance_test = vlc.Instance()
-player_test = instance_test.media_player_new()
+instance = vlc.Instance()
+player_test = instance.media_player_new()
 instances = []
 players = []
+fading = False
 
 
 def init(path_in):
@@ -28,7 +29,7 @@ def full_name(filename):
 def is_valid(filename):
     final_name = full_name(filename)
     try:
-        media = instance_test.media_new(final_name)
+        media = instance.media_new(final_name)
         player_test.set_media(media)
         return player_test.will_play()
     except:
@@ -36,13 +37,9 @@ def is_valid(filename):
 
 
 def play_vlc(filename, volume=100, start_time=0, end_time=0, fadeout=0):
+    global fading
+    fading = False
     final_name = full_name(filename)
-
-    instance_name = filename + ":" + str(volume)
-
-    print("New instance created for", instance_name)
-    # instance = instances[instance_name]
-    instance = vlc.Instance()
 
     # Create a new player
     player = instance.media_player_new()
@@ -53,6 +50,7 @@ def play_vlc(filename, volume=100, start_time=0, end_time=0, fadeout=0):
     player.set_media(media)
     # Set volume using
     player.audio_set_volume(volume)
+    vlc.libvlc_audio_set_mute(player, False)
     player.play()
 
     if start_time > 0:
@@ -75,32 +73,38 @@ def stop_player_thread(player, delay, fadeout):
 
 
 def stop_vlc():
+    global fading
+    fading = False
     for player in players:
         player.stop()
     players.clear()
 
 
 def fadeout_vlc(delay, fadeout_players=None):
+    global fading
+
     global players
     if fadeout_players is None:
         fadeout_players = []
         for player in players:
             fadeout_players.append(player)
-        players.clear()
-
+    fading = True
     fadeout_vlc_thread_instance = threading.Thread(target=fadeout_vlc_thread, args=(delay, fadeout_players,))
     fadeout_vlc_thread_instance.start()
 
 
 def fadeout_vlc_thread(delay, fadeout_players):
+    global fading
+
     delay = delay / 1000
     fadeout_start_time = time.time()
     current_volumes = [playerI.audio_get_volume() for playerI in fadeout_players]
-    while time.time() - fadeout_start_time < delay:
+    while time.time() - fadeout_start_time < delay and fading:
         for i, player in enumerate(fadeout_players):
             volume = int((1 - (time.time() - fadeout_start_time) / delay) * current_volumes[i])
             player.audio_set_volume(volume)
         time.sleep(0.01)
+    fading = False
     for player in fadeout_players:
         player.stop()
     fadeout_players.clear()
